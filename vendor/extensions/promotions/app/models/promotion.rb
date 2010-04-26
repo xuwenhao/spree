286483f5cd6900e565f4a7499f125170c597ca5e
David North <davidnorth@railsdog.com>
2010-04-26 10:16:11 +0100

    refactor promotion.eligible, move expiration checks into separate method

--- vendor/extensions/promotions/app/models/promotion.rb
+++ vendor/extensions/promotions/app/models/promotion.rb
@@ -6,12 +6,16 @@ class Promotion < ActiveRecord::Base
   validates_presence_of :code
   
   def eligible?(order)
-    return false if expires_at and Time.now > expires_at
-    return false if usage_limit and promotion_credits.with_order.count >= usage_limit
-    return false if starts_at and Time.now < starts_at
-    # TODO - also check items in the order (once we support product groups for coupons)
-    true
+    # TODO - evalulate rules
+    !expired?
   end
+
+  def expired?
+    starts_at && Time.now < starts_at ||
+    expires_at && Time.now > expires_at || 
+    usage_limit && promotion_credits.with_order.count >= usage_limit
+  end
+
 
   def create_discount(order)
     return if order.promotion_credits.reload.detect { |credit| credit.adjustment_source_id == self.id }
--- vendor/extensions/promotions/test/unit/promotion_test.rb
+++ vendor/extensions/promotions/test/unit/promotion_test.rb
@@ -11,15 +11,15 @@ class PromotionTest < ActiveSupport::TestCase
     
     context "expires_at < now" do
       setup { @promotion.expires_at = Time.now - 1.day }
-      should "not be eligible" do
-        assert !@promotion.eligible?(Factory(:order))
+      should "not be expired" do
+        assert @promotion.expired?
       end
     end
     
     context "expires_at > now" do
       setup { @promotion.expires_at = Time.now + 1.day }
-      should "be eligible" do
-        assert @promotion.eligible?(Factory(:order))
+      should "not be expired" do
+        assert !@promotion.expired?
       end
     end
     
@@ -27,28 +27,28 @@ class PromotionTest < ActiveSupport::TestCase
       setup { @promotion.usage_limit = 1 }
       context "when coupon has already been used" do
         setup { @promotion.create_discount(Factory(:order)) }
-        should "not be eligible" do
-          assert !@promotion.eligible?(Factory(:order))
+        should "be expired" do
+          assert @promotion.expired?
         end
       end
       context "when coupon has not yet been used" do
-        should "be eligible" do
-          assert @promotion.eligible?(Factory(:order))
+        should "not be expired" do
+          assert !@promotion.expired?
         end
       end
     end
     
     context "with starts_at > now" do
       setup { @promotion.starts_at = Time.now + 1.day }
-      should "not be eligible" do
-        assert !@promotion.eligible?(Factory(:order))
+      should "be expired" do
+        assert @promotion.expired?
       end
     end
 
     context "with starts_at < now" do
       setup { @promotion.starts_at = Time.now - 1.day }
-      should "be eligible" do
-        assert @promotion.eligible?(Factory(:order))
+      should "not be expired" do
+        assert !@promotion.expired?
       end
     end
     
