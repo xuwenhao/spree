0207be3a7517dcc6beff4dbced65b47c320d1f19
David North <davidnorth@railsdog.com>
2010-05-02 17:12:59 +0100

    finding promotions potentially applicable to a product

--- vendor/extensions/promotions/promotions_extension.rb
+++ vendor/extensions/promotions/promotions_extension.rb
@@ -57,6 +57,21 @@ class PromotionsExtension < Spree::Extension
 
     end
     
+    Product.class_eval do
+      has_and_belongs_to_many :promotion_rules
+      
+      def possible_promotions
+        rules_with_matching_product_groups = product_groups.map(&:promotion_rules).flatten
+        all_rules = promotion_rules + rules_with_matching_product_groups
+        promotion_ids = all_rules.map(&:promotion_id).uniq
+        Promotion.automatic.scoped(:conditions => {:id => promotion_ids})
+      end
+    end
+    
+    ProductGroup.class_eval do
+      has_many :promotion_rules
+    end
+    
     [Promotion::Rules::ItemTotal, Promotion::Rules::Product, Promotion::Rules::User, Promotion::Rules::FirstOrder].each &:register
 
     [Calculator::FreeShipping].each(&:register)
--- vendor/extensions/promotions/test/factories/promotion_factory.rb
+++ vendor/extensions/promotions/test/factories/promotion_factory.rb
@@ -1,7 +1,11 @@
 Factory.define :promotion do |f|
-  f.code "FOO"
+  f.code nil
   f.combine true
   f.calculator {|r| Factory(:calculator, :calculable => r.instance_eval{@instance}) }
+end
+
+Factory.define :promotion_rule do |r|
+  r.association(:promotion, :factory => :promotion)
 end
 
 Factory.define(:promotion_credit, :class => PromotionCredit) do |f|
