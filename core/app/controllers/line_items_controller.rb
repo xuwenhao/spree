292076294d510224467a608d957f196b66bc68f9
David North <davidnorth@railsdog.com>
2010-08-13 18:44:50 -0500

    removed any uses of update_totals! that shouldn't be necessary and disabled bits of logic that are supposed to ensure correct order state as this logic will be consolidated into the Order model

--- core/app/controllers/line_items_controller.rb
+++ core/app/controllers/line_items_controller.rb
@@ -4,7 +4,6 @@ class LineItemsController < Spree::BaseController
     line_item = LineItem.find(params[:id], :include => :order)
     order = line_item.order
     line_item.destroy
-    order.update_totals!
     redirect_to edit_order_url(order.number)
   end
 
